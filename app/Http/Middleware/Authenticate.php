c9327809ef3ae6eda4a66fdefcf65c19f179f4aa
hazem05924 <hazemdaher123@gmail.com>
2022-07-01 19:23:04 +0300

    first commit

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('view.login','admin,qarson');
+            return route('view.login','admin');
         }
     }
 }
